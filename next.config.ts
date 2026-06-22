359048437583727c114a39d36dd06d797c0e5073
Dhiraj Shrotri <dhiraj.shrotri@truweathersolutions.com>
2026-06-22 16:56:31 -0400

    feat: updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,4 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: 'AIzaSyBZ351kDA8fhF2FQuBVoHkdhlspCCX1LKc',
-NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "forewarn-51763.firebaseapp.com",
-NEXT_PUBLIC_FIREBASE_PROJECT_ID: "forewarn-51763",
-NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "forewarn-51763.firebasestorage.app",
-NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "598369170617",
-NEXT_PUBLIC_FIREBASE_APP_ID: "1:598369170617:web:3d716a5c7b8ef499f9d768",
-NEXT_PUBLIC_FIREBASE_VAPID_KEY: "BLxnUb-S1pTLfHOuBeboel2lyEwzanyVF-I65dr0kDzR0ka10xzny-FBUiAi3C-D3m48-hozIEmPZ7WFw2lwa60"
-  }
-};
-
+const nextConfig: NextConfig = {};
 export default nextConfig;
